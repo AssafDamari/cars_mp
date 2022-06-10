198673c6d56e0326fec9807f61a386da727b1e99
AssafDamari <asafda@gmail.com>
2022-06-10 18:01:27 +0300

    fix burrel dropper

--- scripts/barrel_dropper.gd
+++ scripts/barrel_dropper.gd
@@ -4,4 +4,6 @@ var burrel_prefab = "res://scenes/barrel.tscn"
 
 func _ready():
 	set_as_toplevel(true)
-	$drop_position.add_child(load(burrel_prefab).instance())
+	var burrel_instance = load(burrel_prefab).instance()
+	$drop_position.add_child(burrel_instance)
+	burrel_instance.global_transform.origin.y -= 0.8
