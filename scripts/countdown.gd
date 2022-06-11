cc1b35d9ca350d6a8d18428c09efa76f8151f6f6
AssafDamari <asafda@gmail.com>
2022-06-11 10:36:13 +0300

    added countdown sound

--- scripts/countdown.gd
+++ scripts/countdown.gd
@@ -10,25 +10,30 @@ func start_countdown():
 	counter_label.visible = true
 	current_countdown_number = 3
 	counter_label.text = str(current_countdown_number)	
+	$bip.play()
 	$timer.start()
 
 
 func _on_timer_timeout():
 	current_countdown_number -= 1
-	if current_countdown_number < 1:
+	
+	if current_countdown_number == 0:
 		counter_label.text = "GO!"
+		$bop.play()
 		SignalManager.emit_signal("countdown_finished", current_countdown_number)
-	else:
+	elif current_countdown_number > 0:
 		counter_label.text = str(current_countdown_number)	
-		
-	# make sure the "GO!" is visible by hiding and stopping in -1
-	if current_countdown_number < 0:
+		$bip.play()
+	elif current_countdown_number < 0:
+		# make sure the "GO!" is visible by hiding and stopping in -1
 		$timer.stop()
 		counter_label.visible = false
+		$bip.stop()
+		$bop.stop()
 		
-	
-	
+		
 func reset():
 	current_countdown_number = 3
 	counter_label.text = str(current_countdown_number)
 	counter_label.visible = false	
+	
